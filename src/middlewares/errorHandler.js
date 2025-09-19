79417c43d5295e4f2e794dffc760332bf4a55f3b
Alena Havalehko <126282451+AlenaHavaleshko@users.noreply.github.com>
2025-09-19 19:11:35 +0300

    hw3

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,19 @@
+import { HttpError } from  'http-errors';
+
+export const errorHandler = (err, req, res, next ) => {
+
+  if (HttpError(err)) {
+    res.status (err.status). json ({
+       status: err.status,
+       message: err.name,
+       data: err,
+    });
+    return ;
+  }
+
+  res.status( 500 ).json({
+     status: 500,
+     message: 'Something went wrong' ,
+     data: err.message,
+  });
+};
--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ import { initMongoConnection } from './db/initMongoConnection.js';
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import { getContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import {errorHandler} from  './middlewares/errorHandler.js' ;
+import { notFoundHandler } from  './middlewares/notFoundHandler.js' ;
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,59 +17,14 @@ app.use(cors());
 // //  pino-http для логування
 app.use(pino());
 
-// Роут для отримання контактів
-app.get('/contacts', async (req, res) => {
-  const contact = await getContacts();
+// Мідлварка,щоб розпарсити обьект боді
+app.use(express.json());
 
-  res.json({
-    status: 200,
-    message: 'Contacts get successfully!',
-    data: contact, //додати реальні дані з БД
-  });
-});
+app.use('/contacts', contactsRouter);  // Додаємо роутер до app як middleware
 
-// Роут для отримання контакту по id
-app.get("/contacts/:id", async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const contact = await getContactById(id);
-
-    if (!contact) {
-      return res.status(404).json({
-        message: "Contact not found",
-      });
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${id}!`,
-      data: contact,
-    });
-
-  } catch (error) {
-    console.error("Error while getting contact:", error);
-    res.status(500).json({ message: "Server error" });
-  }
-
-  // const contact = await Contact.findById(id);
-
-  // console.log(contact);
-
-  //   res.json({
-  //     status:200,
-  //     message: "Contact get successfully",
-  //     data: {
-  //       id,
-  //       name: `Contact ${id}`
-  //     },
-  //   });
-});
-
-// Обробник для неіснуючих маршрутів
-app.use((req, res, next) => {
-  res.status(404).json({ status: 404, message: "Not found" });
-});
+// Handle 404
+app.use( notFoundHandler);
+app.use(errorHandler);
 
 // Підключення до MongoDB
 export async function setupServer() {
